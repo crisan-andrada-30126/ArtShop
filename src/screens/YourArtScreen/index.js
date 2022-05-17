1504724817b232a5bb83f45e0f0acdf7eb77edd6
crisan-andrada-30126 <61448551+crisan-andrada-30126@users.noreply.github.com>
2022-05-17 17:52:50 +0300

    no artworks for sell yet

--- src/screens/YourArtScreen/index.js
+++ src/screens/YourArtScreen/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet, ScrollView, FlatList, ImageBackground } from 'react-native'
+import { View, Text, Image, StyleSheet, ScrollView, FlatList, ImageBackground, Pressable } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import database from '@react-native-firebase/database';
 import Picture from '../../images/pictur.jpg'
@@ -7,11 +7,13 @@ import Background from '../../images/paintBlue.jpg'
 import { getItemsByUserId } from '../../api/ArtApi'
 import firestore from '@react-native-firebase/firestore';
 import ArtItem from './item';
+import { useNavigation } from '@react-navigation/native';
 
 const artsCollection = firestore().collection('Artworks');
 
 
 const YourArtScreen = (props) => {
+    const navigation = useNavigation()
     const [items, setItems] = useState([])
 
     useEffect(() => {
@@ -43,11 +45,26 @@ const YourArtScreen = (props) => {
 
         <View style={styles.container}>
             <ImageBackground source={Background} resizeMode="cover" style={styles.image}>
-                <FlatList data={items}
+                {items.length > 0 ? <FlatList data={items}
                     renderItem={({ item }) => <ArtItem product={item} />}
                     showsVerticalScrollIndicator={false}
                     showsHorizontalScrollIndicator={false}
-                />
+                /> :
+                    <View style={styles.btn}>
+                        <Pressable onPress={() => navigation.navigate('SellScreen')}
+                            style={styles.add}>
+                            <Text style={styles.addText}>
+                                +
+                            </Text>
+                        </Pressable>
+                        <Text style={styles.textAdd}>
+                            You have not added anyting yet!
+                        </Text>
+                        <Text style={styles.textAdd}>
+                            Add your artwork!
+                        </Text>
+                    </View>
+                }
             </ImageBackground>
         </View>
 
@@ -61,6 +78,33 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         width: '100%'
+    },
+    btn: {
+        margin: '0 auto',
+        width: '50%',
+
+
+    },
+    add: {
+        marginTop: 15,
+        width: 50,
+        height: 50,
+        backgroundColor: '#E59EAA',
+        borderRadius: 100,
+        alignItems: 'center',
+
+
+    },
+    addText: {
+        marginTop: -12,
+        fontSize: 50,
+        color: '#fff',
+
+    },
+    textAdd: {
+        color: '#DB7093',
+        fontWeight: 'bold',
+        textAlign: 'center'
     },
 });
 
