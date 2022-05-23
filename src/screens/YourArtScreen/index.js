cd167c60b6fb4b25bebeb74f0e0ce032169c5db7
crisan-andrada-30126 <61448551+crisan-andrada-30126@users.noreply.github.com>
2022-05-24 00:21:53 +0300

    see your art for sale + delete

--- src/screens/YourArtScreen/index.js
+++ src/screens/YourArtScreen/index.js
@@ -4,20 +4,65 @@ import database from '@react-native-firebase/database';
 import Picture from '../../images/pictur.jpg'
 import ProductItem from '../../components/ProductItem';
 import Background from '../../images/paintBlue.jpg'
+import { getItemsByUserId } from '../../api/ArtApi'
+import firestore from '@react-native-firebase/firestore';
+import ArtItem from './item';
+
+const artsCollection = firestore().collection('Artworks');
 
 
-const reference = database().ref('/Paintings');
+const YourArtScreen = (props) => {
+    const [items, setItems] = useState([])
+    const [changed, setChanged] = useState(false)
+
+    useEffect(() => {
+        var responselist = []
+        artsCollection.get().then(querySnapshot => {
+
+            querySnapshot.forEach(documentSnapshot => {
+
+                if (props.user.uid == documentSnapshot.data().userId) {
+                    responselist.push(documentSnapshot.data())
+                }
 
 
-const YourArtScreen = () => {
+            })
 
+            setItems(responselist)
+        });
+
+
+    }, []);
+
+    useEffect(() => {
+        var responselist = []
+        artsCollection.get().then(querySnapshot => {
+
+            querySnapshot.forEach(documentSnapshot => {
+
+                if (props.user.uid == documentSnapshot.data().userId) {
+                    responselist.push(documentSnapshot.data())
+                }
+
+
+            })
+
+            setItems(responselist)
+        });
+
+
+    }, [changed]);
 
     return (
 
 
         <View style={styles.container}>
             <ImageBackground source={Background} resizeMode="cover" style={styles.image}>
-                <Text> YourArtScreen</Text>
+                <FlatList data={items}
+                    renderItem={({ item }) => <ArtItem product={item} setChanged={setChanged} changed={changed} />}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                />
             </ImageBackground>
         </View>
 
